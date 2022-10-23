7c1b18cf63d0eb68b3a74c19113b62c492659642
phinic925 <phinicnyangate480@gmail.com>
2022-10-23 14:37:54 +0100

    add tole to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+
+  before_create :set_default_role
+
+  before_save { |user| user.username = user.username.downcase }
+  
+  enum role: [:admin, :job_seeker, :employer] 
+  after_initialize :set_default_role, :if => :new_record?
+  
+
+  def set_default_role
+   self.role ||= :admin
+  end
 end
