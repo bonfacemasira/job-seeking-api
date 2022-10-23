6838e65e096bfc425ecae38f99c95bb30a1ccbb9
phinic925 <phinicnyangate480@gmail.com>
2022-10-23 14:22:15 +0100

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "active_model/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
@@ -35,5 +35,10 @@ module JobSeekingApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+    config.action_dispatch.cookies_same_site_protection = :strict
+
   end
 end
