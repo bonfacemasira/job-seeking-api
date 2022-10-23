85aeb6b906a56928aae8a4b71b0278ed67e8e2c6
phinic925 <phinicnyangate480@gmail.com>
2022-10-23 14:35:15 +0100

    create user

---
+++ app/controllers/user/Registrations_controller.rb
@@ -0,0 +1,30 @@
+class User::RegistrationsController < Devise::RegistrationsController
+    wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+   
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
+
+    def create
+        user = User.create!(user_params)
+       
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+
+
+    private
+   
+    def user_params
+        params.permit(:username, :role, :email, :password, :password_confirmation)
+    end
+    
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+end
