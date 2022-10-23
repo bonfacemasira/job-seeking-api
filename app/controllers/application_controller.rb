14afd8b7e9dc5a18b540fa21344b6f9f5998a62a
phinic925 <phinicnyangate480@gmail.com>
2022-10-23 14:39:53 +0100

    add signup permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    respond_to :json
+    protected
+  
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation ) }
+        devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit( :username, :email, :password, :remember_me) }
+
+    end
 end
