96b57f2b4547c568958e4ee22bc916b803629551
phinic925 <phinicnyangate480@gmail.com>
2022-10-23 14:33:12 +0100

    create users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users,:controllers => {
+    :registrations => 'user/registrations', :defaults => { :format => 'json' },
+}
+ resources :users, only: [:index, :show, :destroy]
+ resources :sessions, only: [:create, :destroy]
+  
 end
