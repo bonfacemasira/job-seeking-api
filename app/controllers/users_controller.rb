0d31b6cadb2dd1d97d71ba09e51a94d2b438b527
phinic925 <phinicnyangate480@gmail.com>
2022-10-24 13:00:26 +0100

    give admin priviledges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only:[:show]
+    before_action :authenticate_user!
+    before_ction :admin_only, :except[:show]
 
     def show
         user = User.find_by(id: session[:user_id])
@@ -14,4 +15,25 @@ class UsersController < ApplicationController
         user = User.all
         render json: user
     end
+    def update
+        @user = User.find(params[:id])
+        authorize @user
+        if @user.update_attributes(secure_params)
+          redirect_to users_path, :notice => "User updated."
+        else
+          redirect_to users_path, :alert => "Unable to update user."
+        end
+      end
+    
+      def destroy
+        user = User.find(params[:id])
+        authorize user
+        user.destroy
+        redirect_to users_path, :notice => "User deleted."
+      end
+    private
+    def admin_only
+        unless current_user.admin?
+          redirect_to :back, :alert => "Access denied."
+        end
 end
