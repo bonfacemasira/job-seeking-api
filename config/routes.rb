3f275d71449e321450db45bf8d80198bb54c111c
phinic925 <phinicnyangate480@gmail.com>
2022-10-24 10:17:54 +0100

    creates job_seeker route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :job_seekers
+
   devise_for :users,:controllers => {
     :registrations => 'user/registrations', :defaults => { :format => 'json' },
 }
  resources :users, only: [:index, :show, :destroy]
  resources :sessions, only: [:create, :destroy]
-  
+ resources :job_seekers
 end
--- db/migrate/20221024091421_create_job_seekers.rb
+++ db/migrate/20221024091421_create_job_seekers.rb
@@ -2,11 +2,11 @@ class CreateJobSeekers < ActiveRecord::Migration[7.0]
   def change
     create_table :job_seekers do |t|
       t.string :full_name
-      t.string :salary_expectation
+      t.integer :salary_expectation
       t.string :certficates
       t.string :cv
       t.boolean :availability
-      t.string :user_id
+      t.integer :user_id
 
       t.timestamps
     end
