cd89794b0d7dc2d0a2fde1de48500a45c1f2dc2a
principalkelvo <principalkelvo@gmail.com>
2022-11-01 16:35:35 +0300

    last schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_30_140400) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_085821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,7 +106,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_140400) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "current_login_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["role"], name: "index_users_on_role"
     t.index ["username"], name: "index_users_on_username", unique: true
