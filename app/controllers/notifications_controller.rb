9ef44432f0b84adff14d8d2a988a8d2c4367444e
mbolonzi-design <mbolonzi.design@gmail.com>
2022-11-01 13:06:50 +0300

    create controller routes to render notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,31 @@
 class NotificationsController < ApplicationController
+    def index
+        @notifications = Notification.where(user: current_user).unread
+    end
+
+    def create
+        @notification = Notification.new(notification_params)
+        @notification.sender = current_user
+        @notification.save
+        redirect_to notifications_path
+    end
+    
+    def mark_as_read
+        @notifications = Notification.where(user: current_user).unread
+        @notifications.update_all(read_at: Time.zone.now)
+        redirect_to notifications_path
+    end
+
+    def destroy
+        @notification = Notification.find(params[:id])
+        @notification.destroy
+        redirect_to notifications_path
+    end
+
+    private
+
+    def notification_params
+        params.require(:notification).permit(:user_id, :status)
+    end
 end
+
